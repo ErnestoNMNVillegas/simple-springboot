1a7f729de9983eb0df8c5d1b89fd5ea50f98d19f
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-08-19 18:57:47 -0500

    book controller

--- src/main/java/com/simple/simplespringboot/controller/BookController.java
+++ src/main/java/com/simple/simplespringboot/controller/BookController.java
@@ -1,13 +1,35 @@
 package com.simple.simplespringboot.controller;
 
+import com.simple.simplespringboot.model.Book;
 import com.simple.simplespringboot.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
     @Autowired
     private BookRepository bookRepository;
+
+    @GetMapping
+    public Iterable findAll() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping("/title/{bookTitle}")
+    public List findByTitle(@PathVariable String bookTitle) {
+        return bookRepository.findByTitle(bookTitle);
+    }
+
+    @GetMapping("/{id}")
+    public Book findOne(@PathVariable Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(BookNotFoundException::new);
+    }
+
 }
