6653b328f0a6bb0bf3ee9611901997f32dd3dd14
Ernesto Villegas <ernesto.nmn.villegas@gmail.com>
2023-08-26 22:33:31 -0500

    test

---
+++ src/test/java/com/simple/simplespringboot/model/SpringContextTest.java
@@ -0,0 +1,14 @@
+package com.simple.simplespringboot.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringContextTest {
+    @Test
+    public void contextLoads() {
+    }
+}
